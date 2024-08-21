ae047b1f49cc2aeaa5e9fba98f991c1a49c2e5ae
Neat11 <hello.astitva@gmail.com>
2024-08-22 01:39:46 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/Courses" element={<Courses />} />
+      <Route path="/Packages" element={<Courses />} />
     </Routes>
     </BrowserRouter>
   <Footer/>
